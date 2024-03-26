e5d6b3a4e26d35ced7ca23c493e726d0451c66fb
praniil <pparajuli15@gmail.com>
2024-03-26 11:49:42 +0545

    feat: performed the same sine curve's sketching using different method of drawing it

--- plotter/curvePlotter.go
+++ plotter/curvePlotter.go
@@ -13,15 +13,23 @@ func Plot() {
 	debug := false
 	plot, _ := glot.NewPlot(dimension, persist, debug)
 	plot.SetTitle("Pranil's Plotter")
-	fct := func(x float64) float64 { return (math.Sin(x)) }
+	// fct := func(x float64) float64 { return (math.Sin(x)) }
 	groupName := "Sine Curve"
 	style := "lines"
-	pointsX := make([]float64, 500)
-	for i := 0.0; i <= 11.17011; i = i + 0.01 {
+	pointsX := make([]float64, 0, 100) //type, length, capacit:= length 0 aaile ko so no elements there, capacity can have this much initial capacity
+	for i := 0.0; i <= 11.17011; i = i+0.01 {
 		pointsX = append(pointsX, i)
 	}
+	pointsY := make([]float64, 0, 100)
+	for _, pointX := range pointsX {
+		pointsY = append(pointsY, math.Sin(pointX))
+	}
+	points := [][]float64{pointsX, pointsY}
+	plot.AddPointGroup(groupName, style, points)
+	fmt.Println(pointsX)
+	fmt.Println(pointsY)
 	// pointsX := []float64{0, 0.5235988, 1.047198, 1.570796, 2.094395, 3.141593}
-	plot.AddFunc2d(groupName, style, pointsX, fct)
+	// plot.AddFunc2d(groupName, style, pointsX, fct)
 
 	fileName := "plot.png"
 	err := plot.SavePlot(fileName)
